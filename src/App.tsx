2e9a3c39b409eb332e745ec5f41d8ebc5c17cde4
BackupMiles <leanwithmiles@gmail.com>
2023-03-16 21:25:48 +0100

    changed images path

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className="main">
-        <Card imgSrc='../images/image-qr-code.png' subtitle={subtitle} title={title} />
+        <Card imgSrc='/images/image-qr-code.png' subtitle={subtitle} title={title} />
       </div>
     </div>
   )
